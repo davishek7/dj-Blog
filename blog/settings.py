b4c48c35a5eb31356923ffbd612e02a0316e1223
Avishek Das <davishek7@gmail.com>
2021-04-02 07:22:55 +0530

    Allowed hosts added

--- blog/settings.py
+++ blog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','avishekdjangoblog.herokuapp.com']
 
 
 # Application definition
