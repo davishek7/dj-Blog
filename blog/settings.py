cb668442b652a34ec97800af0ea43e86c73598d5
Avishek Das <davishek7@gmail.com>
2021-05-09 00:04:50 +0530

    Theme fixing

--- blog/settings.py
+++ blog/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','avishekblog.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','avishekdjangoblog.herokuapp.com']
 
 
 INSTALLED_APPS = [
